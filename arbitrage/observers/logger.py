09633681f73898583473e60b160c2b603a405054
danielj86 <daniel@budsoft.co.il>
2014-07-30 18:59:44 +0300

    asd

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -1,8 +1,20 @@
 import logging
+import httplib
+import urllib
+import config
 from .observer import Observer
 
 
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
+        params = {'message': message}
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("jany.co.il:80")
+        conn.request("POST", "/contact", params, headers)
+        response = conn.getresponse()
+        data = response.read()
+        logging.warn("data")
+        logging.warn(data)
+        conn.close()			
         logging.info("profit: %f USD with volume: %f BTC - buy at %.4f (%s) sell at %.4f (%s) ~%.2f%%" % (profit, volume, buyprice, kask, sellprice, kbid, perc))
