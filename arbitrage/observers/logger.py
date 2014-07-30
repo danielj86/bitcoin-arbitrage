256d5d668ebc98246b3fcd85fefec13457f48e83
danielj86 <daniel@budsoft.co.il>
2014-07-30 19:26:02 +0300

    jkhkh

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -9,7 +9,7 @@ class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
         params = urllib.urlencode({'message': "hello"})
-		json_dump = json.dumps(params)
+        json_dump = json.dumps(params)
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = http.client.HTTPConnection("jany.co.il:80")
         conn.request("POST", "/contact", json_dump, headers)
