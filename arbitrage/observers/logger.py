63d3ed213abb472d1974a28a1f558ea33fe6c8bd
danielj86 <daniel@budsoft.co.il>
2014-07-30 19:10:52 +0300

    asd

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -8,7 +8,7 @@ from .observer import Observer
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
-        params = {'message': message}
+        params = {'message': "hello"}
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = http.client.HTTPConnection("jany.co.il:80")
         conn.request("POST", "/contact", params, headers)
