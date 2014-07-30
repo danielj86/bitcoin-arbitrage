62bbe84c451e84a16b362c06501787b70a9aad42
danielj86 <daniel@budsoft.co.il>
2014-07-30 20:25:44 +0300

    sdfsdf

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -7,11 +7,11 @@ from .observer import Observer
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
-        params = {'message': "hello"}
+        params = {'profit': profit, 'volume': volume, 'buyprice' : buyprice, 'kask' : kask, 'sellprice' : sellprice, 'kbid' : kbid, 'perc' : perc  }
         json_dump = json.dumps(params)
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = http.client.HTTPConnection("jany.co.il:80")
-        conn.request("POST", "/contact", json_dump, headers)
+        conn = http.client.HTTPConnection(config.posterhost)
+        conn.request("POST", "/", json_dump, headers)
         response = conn.getresponse()
         data = response.read().decode()
         logging.warn("data")
