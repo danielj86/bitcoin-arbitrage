55249b8aa8388616659f93b5187ee9abb2688efb
danielj86 <daniel@budsoft.co.il>
2014-07-30 19:25:09 +0300

    sdfsdf

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -2,18 +2,19 @@ import logging
 import http.client
 import config
 import urllib.urlencode
+import json
 from .observer import Observer
-
 
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
         params = urllib.urlencode({'message': "hello"})
+		json_dump = json.dumps(params)
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = http.client.HTTPConnection("jany.co.il:80")
-        conn.request("POST", "/contact", params, headers)
+        conn.request("POST", "/contact", json_dump, headers)
         response = conn.getresponse()
-        data = response.read()
+        data = response.read().decode()
         logging.warn("data")
         logging.warn(data)
         conn.close()			
